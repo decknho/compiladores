b30f3ee3f7510015e183e439f2b305b7b175d7c4
Dereck Eder Cabral Santana <decknho26@gmail.com>
2026-08-24 18:48:52 -0300

    lexer.py

--- lexer.py
+++ lexer.py
@@ -1,12 +1,22 @@
 import re
 
 
+PALAVRAS_RESERVADAS = {
+    "if": "IF",
+    "while": "WHILE",
+    "begin": "BEGIN",
+    "set": "SET",
+    "print": "PRINT"
+}
+
+
 def lexer(codigo):
     tokens = []
 
     padrao = r"""
         (?P<ESPACO>\s+)
-        |(?P<NUMERO>\d+(?:\.\d+)?)
+        |(?P<FLOAT>\d+\.\d+)
+        |(?P<INTEGER>\d+)
         |(?P<OPERADOR>==|!=|<=|>=|[+\-*/%<>])
         |(?P<LPAR>\()
         |(?P<RPAR>\))
@@ -14,26 +24,52 @@ def lexer(codigo):
         |(?P<INVALIDO>.)
     """
 
+    linha = 1
+
     for match in re.finditer(padrao, codigo, re.VERBOSE):
         tipo = match.lastgroup
         lexema = match.group()
 
+        # Conta quantas linhas existem no trecho encontrado
         if tipo == "ESPACO":
+            linha += lexema.count("\n")
             continue
 
+        # Caractere que não pertence à linguagem
         if tipo == "INVALIDO":
             raise Exception(
-                f"Erro Léxico: caractere inválido '{lexema}'"
+                f"Erro Léxico: Caractere inválido "
+                f"'{lexema}' na linha {linha}."
             )
 
-        tokens.append((tipo, lexema))
+        # Identifica palavras reservadas
+        if tipo == "ID" and lexema in PALAVRAS_RESERVADAS:
+            tipo = PALAVRAS_RESERVADAS[lexema]
+
+        # Padroniza operadores
+        if tipo == "OPERADOR":
+            tipo = "OP"
+
+        tokens.append({
+            "token": tipo,
+            "lexema": lexema,
+            "linha": linha
+        })
 
     return tokens
 
-
-codigo = "(print (+ 10 20))"
+codigo = """
+(begin
+    (set idade 20)
+    (print (+ idade 5))
+)
+"""
 
 tokens = lexer(codigo)
 
-for token, lexema in tokens:
-    print(f"{token:<12} {lexema}")
+for token in tokens:
+    print(
+        f"{token['token']:<10}"
+        f"{token['lexema']:<10}"
+        f"linha {token['linha']}"
+    )
